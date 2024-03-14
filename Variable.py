9029bb15646f237e5b8102dc438962f4d008bb17
codingbyasish <161559490+codingbyasish@users.noreply.github.com>
2024-03-14 21:28:27 +0530

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,17 @@
+# Keyword and Identifier
+# Keyword - reserved words.
+# Variable name = Variable value
+
+age = 45
+print(age)
+age = "codingbyasish"
+print(age)
+age = 'C'
+print(age)
+
+# class = "codingbyasish"
+_123 = 789
+print(_123)
+
+_ = "Amit"
+print(_)
