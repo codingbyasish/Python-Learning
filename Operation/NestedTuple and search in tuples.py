df86c7da9e1af67300799c3b712c444f9471fb54
codingbyasish <161559490+codingbyasish@users.noreply.github.com>
2024-03-15 08:46:21 +0530

    NestedTuple and search in tuples.py

---
+++ Operation/NestedTuple and search in tuples.py
@@ -0,0 +1,13 @@
+
+# Nested Tuples
+hero1 = ("Batman", "Bruce Wayne")
+hero2 = ("Wonder Woman", "Diana Prince")
+awesome_team = (hero1, hero2)
+print(awesome_team)
+print(awesome_team[0][1])
+print(awesome_team[1][1])
+
+# Search in Tuple
+cities = ("London", "Paris", "Los Angeles", "Tokyo")
+print("Paris" in cities)
+print("Moscow" in cities)
