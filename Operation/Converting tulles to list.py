391aee21dcda5874eb4f1c6691df91ffcb92292b
codingbyasish <161559490+codingbyasish@users.noreply.github.com>
2024-03-15 08:45:13 +0530

    Create Converting tulles to list.py

---
+++ Operation/Converting tulles to list.py
@@ -0,0 +1,10 @@
+
+hero1 = ("Batman", "Bruce Wayne")
+hero2 = ("Wonder Woman", "Diana Prince")
+awesome_team = hero1 + hero2
+print(awesome_team)
+print(list(awesome_team))
+
+# Convert to List
+my_tuple = (1, 2, 3, 4, 5)
+print(list(my_tuple))
